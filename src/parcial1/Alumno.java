d668afc722ae6336886ccd4693e90fb6b1573f3b
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-12-12 21:17:55 -0300

    Validaciones para campos de clase Alumno.

--- src/parcial1/Alumno.java
+++ src/parcial1/Alumno.java
@@ -9,21 +9,39 @@ public class Alumno {
 	private String apellido;
 
 	public Alumno(int nroLibreta, String nombre, String apellido) {
-		this.nroLibreta = nroLibreta;
-		this.nombre = nombre;
-		this.apellido = apellido;
+		setNroLibreta(nroLibreta);
+		setNombre(nombre);
+		setApellido(apellido);
 	}
 
 	public int getNroLibreta() {
 		return nroLibreta;
 	}
 
+	public void setNroLibreta(int nroLibreta) {
+		if (nroLibreta <= 0)
+			throw new IllegalArgumentException("El númeor de libreta debe ser positivo");
+		this.nroLibreta = nroLibreta;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
 
+	public void setNombre(String nombre) {
+		if (nombre.length() < 3)
+			throw new IllegalArgumentException("El nombre debe tener al menos 3 caracteres");
+		this.nombre = nombre;
+	}
+
 	public String getApellido() {
 		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		if (apellido.length() < 3)
+			throw new IllegalArgumentException("El apellido debe tener al menos 3 caracteres");
+		this.apellido = apellido;
 	}
 
 	@Override
