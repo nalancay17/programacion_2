751825ac62f4dd50f1a449916a986f4be3d78474
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-12-12 21:18:05 -0300

    Clase SistemaInscripcion y método para registrar nuevos alumnos al sist.

---
+++ src/parcial1/AlumnoExistenteException.java
@@ -0,0 +1,11 @@
+package parcial1;
+
+public class AlumnoExistenteException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AlumnoExistenteException(String mensaje) {
+		super(mensaje);
+	}
+
+}
---
+++ src/parcial1/SistemaInscripcion.java
@@ -0,0 +1,25 @@
+package parcial1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class SistemaInscripcion {
+
+	private Set<Alumno> alumnos;
+	private Map<String, List<Comision>> materiasYComisiones;
+
+	public SistemaInscripcion() {
+		this.alumnos = new HashSet<Alumno>();
+		this.materiasYComisiones = new HashMap<String, List<Comision>>();
+	}
+
+	public void registrarAlumno(int nroLibreta, String nombre, String apellido) throws Exception {
+		Alumno alumno = new Alumno(nroLibreta, nombre, apellido);
+		if (!alumnos.add(alumno))
+			throw new AlumnoExistenteException("El alumno ya se encuentra registrado");
+	}
+
+}
